b357922d809ea82bb6848bc3f9984b1f9fc5f6ca
duxdevenexia <88348139+duxdevenexia@users.noreply.github.com>
2024-10-31 12:01:37 -0400

    10/31/2024, class activity - CoroutineConversion

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,10 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +25,19 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
+    // Use a coroutine scope
+    private val coroutineScope = MainScope() // This will handle lifecycle
+
+
+    /*
     val handler = Handler(Looper.getMainLooper(), Handler.Callback {
 
         currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
         cakeImageView.alpha = it.what / 100f
         true
     })
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,4 +52,41 @@ class MainActivity : AppCompatActivity() {
             }.start()
         }
     }
+
+    */
+
+
+   // /*
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // after click teh button
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            launchOpacityAnimation()    // call launchOpacityAnimation()
+        }
+    }
+
+
+    private fun launchOpacityAnimation() {
+        // create a new coroutine
+        coroutineScope.launch {
+            // by repeat
+            repeat(100) { i ->
+                // changing of the textView and ImageView
+                currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", i)
+                cakeImageView.alpha = i / 100f
+                delay(40) // Coroutine delay instead of Thread.sleep
+            }
+        }
+    }
+
+    // fun for canceling the coroutineScope
+    override fun onDestroy() {
+        super.onDestroy()
+        // Cancel the coroutine scope to prevent memory leaks
+        coroutineScope.cancel()
+    }
+
+    // */
 }
